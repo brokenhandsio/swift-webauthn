cda9a5cc699eb04fb261ea6fda43e24aff2920df
Chris Salvador <csalvador58@gmail.com>
2025-09-29 22:06:48 -0700

    Convert AttestationConveyancePreference from enum to struct

--- Sources/WebAuthn/Ceremonies/Registration/AttestationConveyancePreference.swift
+++ Sources/WebAuthn/Ceremonies/Registration/AttestationConveyancePreference.swift
@@ -14,10 +14,34 @@
 /// Options to specify the Relying Party's preference regarding attestation conveyance during credential generation.
 ///
 /// Currently only supports `none`.
-public enum AttestationConveyancePreference: String, Encodable, Sendable {
+public struct AttestationConveyancePreference: RawRepresentable, Equatable, Hashable, Encodable, Sendable {
+    public let rawValue: String
+    
+    public init?(rawValue: String) {
+        switch rawValue {
+        case "none":
+            self.rawValue = rawValue
+        default:
+            return nil
+        }
+    }
+    
+    private init(_ rawValue: String) {
+        self.rawValue = rawValue
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(rawValue)
+    }
+    
     /// Indicates the Relying Party is not interested in authenticator attestation.
-    case none
-    // case indirect
-    // case direct
-    // case enterprise
+    public static let none = AttestationConveyancePreference("none")
+//    public static let indirect = AttestationConveyancePreference("indirect")
+//    public static let direct = AttestationConveyancePreference("direct")
+//    public static let enterprise = AttestationConveyancePreference("enterprise")
+}
+
+extension AttestationConveyancePreference: CustomStringConvertible {
+    public var description: String { rawValue }
 }
