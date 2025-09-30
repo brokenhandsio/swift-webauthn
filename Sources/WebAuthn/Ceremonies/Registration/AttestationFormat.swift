fd6db581f512cda8e8662d012fe2ee817a04a3f8
Chris Salvador <csalvador58@gmail.com>
2025-09-29 22:08:53 -0700

    Convert AttestationFormat from enum to struct

--- Sources/WebAuthn/Ceremonies/Registration/AttestationFormat.swift
+++ Sources/WebAuthn/Ceremonies/Registration/AttestationFormat.swift
@@ -11,12 +11,41 @@
 //
 //===----------------------------------------------------------------------===//
 
-public enum AttestationFormat: String, RawRepresentable, Equatable, Sendable {
-    case packed
-    case tpm
-    case androidKey = "android-key"
-    case androidSafetynet = "android-safetynet"
-    case fidoU2F = "fido-u2f"
-    case apple
-    case none
+public struct AttestationFormat: RawRepresentable, Equatable, Hashable, Codable, Sendable {
+    public let rawValue: String
+    
+    public init?(rawValue: String) {
+        switch rawValue {
+            case "packed", "tpm", "android-key", "android-safetynet", "fido-u2f", "apple", "none":
+            self.rawValue = rawValue
+        default:
+            return nil
+        }
+    }
+    
+    private init(_ rawValue: String) {
+        self.rawValue = rawValue
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        rawValue = try container.decode(String.self)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(rawValue)
+    }
+    
+    public static let packed = AttestationFormat("packed")
+    public static let tpm = AttestationFormat("tpm")
+    public static let androidKey = AttestationFormat("android-key")
+    public static let androidSafetynet = AttestationFormat("android-safetynet")
+    public static let fidoU2F = AttestationFormat("fido-u2f")
+    public static let apple = AttestationFormat("apple")
+    public static let none = AttestationFormat("none")
+}
+
+extension AttestationFormat: CustomStringConvertible {
+    public var description: String { rawValue }
 }
